5c6c1f559e1606d2739f50959e7079edb0f3b6b3
haibara <haibara406@gmail.com>
2025-09-19 23:28:23 +0800

    first commit: 初始化项目

---
+++ cdu-snzh/src/main/java/com/snzh/CduSnzhApplication.java
@@ -0,0 +1,13 @@
+package com.snzh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CduSnzhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CduSnzhApplication.class, args);
+    }
+
+}
---
+++ cdu-snzh/src/test/java/com/snzh/CduSnzhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snzh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CduSnzhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
